773c35b33c5a96b785a16568e319a4a6be606abc
David Fuentes <davidf0827@gmail.com>
2020-08-15 00:23:12 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -17,7 +17,7 @@ class Transfer
   end
   
   def execute_transaction 
-    if @sender. < @amount 
+    if @sender.balance < @amount 
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
 
